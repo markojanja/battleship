db44ad8c0bef1d3c89e0c61648f0816199e4bd42
Marko Janjic <marko.m.janjic@gmail.com>
2023-07-30 12:32:44 +0200

    added computer to the player tests

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -4,5 +4,7 @@ describe('player tests', () => {
   test('test player name', () => {
     const player = new Player('player');
     expect(player.name).toBe('player');
+    const computer = new Player('computer');
+    expect(computer.name).toBe('computer');
   });
 });
